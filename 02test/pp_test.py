d808623f985ffcb5480610dc313e0d89fb30e7b2
Osamu Mizuno <o-mizuno@kit.ac.jp>
2023-03-16 18:01:36 +0900

    fix bug

--- 02test/pp_test.py
+++ 02test/pp_test.py
@@ -60,7 +60,7 @@ def common_task(mpl_file, out_file):
 TEST_RESULT_DIR = "test_results"
 TEST_EXPECT_DIR = "test_expects"
 
-test_data = sorted(glob.glob("../input0{1,2}/*.mpl", recursive=True))
+test_data = sorted(glob.glob("../input0[12]/*.mpl", recursive=True))
 
 @pytest.mark.timeout(10)
 @pytest.mark.parametrize(("mpl_file"), test_data)
