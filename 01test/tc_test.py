aa5e182b7f107743036b938c469350b16f4b33cf
Osamu Mizuno <o-mizuno@kit.ac.jp>
2023-03-23 13:26:32 +0900

    test expects for kadai01

--- 01test/tc_test.py
+++ 01test/tc_test.py
@@ -7,13 +7,17 @@ import sys
 import re
 from pathlib import Path
 
+class ScanError(Exception):
+    pass
+
 def command(cmd):
     try:
         result = subprocess.run(cmd, shell=True, check=False,
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                 universal_newlines=True)
-        for line in result.stdout.splitlines():
-            yield line
+#        for line in result.stdout.splitlines():
+#            yield line
+        return [result.stdout,result.stderr]
     except subprocess.CalledProcessError:
         print('外部プログラムの実行に失敗しました [' + cmd + ']', file=sys.stderr)
         sys.exit(1)
@@ -21,15 +25,30 @@ def command(cmd):
 def common_task(mpl_file, out_file):
     try:
 #        tc = Path(__file__).parent.parent.joinpath("tc")
-        tc = Path("/workspaces").joinpath("tc")
-        compiler_text = command("{} {}".format(tc,mpl_file))
+        exec = Path("/workspaces").joinpath("tc")
+        exec_res = command("{} {}".format(exec,mpl_file))
         out = []
-        for line in compiler_text:
+        sout = exec_res.pop(0)
+        serr = exec_res.pop(0)
+        if serr:
+            raise ScanError
+        for line in sout.splitlines():
             out.append(line)
         out.sort()
         with open(out_file, mode='w') as fp:
             for l in out:
                 fp.write(re.sub(r'\s*"\s*(\S*)\s*"\s*(\d+)\s*',r'"\1"\t\2\n',l))
+        return 0
+    except ScanError:
+        if re.search(r'sample0', mpl_file):
+            for line in serr.splitlines():
+                out.append(line)
+            with open(out_file, mode='w') as fp:
+                for l in out:
+                    fp.write(l+'\n')
+            return 1
+        else:
+            raise ScanError        
     except Exception as err:
         with open(out_file, mode='w') as fp:
             print(err, file=fp)
@@ -50,7 +69,12 @@ def test_run(mpl_file):
     if not Path(TEST_RESULT_DIR).exists():
         os.mkdir(TEST_RESULT_DIR)
     out_file = Path(TEST_RESULT_DIR).joinpath(Path(mpl_file).stem + ".out")
-    common_task(mpl_file, out_file)
-    expect_file = Path(TEST_EXPECT_DIR).joinpath(Path(mpl_file).stem + ".txt")
-    with open(out_file) as ofp, open(expect_file) as efp:
-        assert ofp.read() == efp.read()
+    res = common_task(mpl_file, out_file)
+    if res == 0:
+        expect_file = Path(TEST_EXPECT_DIR).joinpath(Path(mpl_file).stem + ".stdout")
+        with open(out_file) as ofp, open(expect_file) as efp:
+            assert ofp.read() == efp.read()
+    else:
+        with open(out_file) as ofp:
+            assert not ofp.read() == ''
+
