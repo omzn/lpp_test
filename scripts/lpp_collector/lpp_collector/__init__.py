ca1f43014b6e6bd3d14b6d05736c4303a89ff0aa
f0reachARR <f0reach@f0reach.me>
2024-07-20 14:36:01 +0900

    collector test

--- scripts/lpp_collector/lpp_collector/__init__.py
+++ scripts/lpp_collector/lpp_collector/__init__.py
@@ -1,2 +1,22 @@
-def pytest_configure(config):  # pragma: no cover
-    print("pytest_lpp_collector: pytest_configure() called")
+from _pytest.config import Config
+from _pytest.reports import TestReport
+
+
+class LppCollector:
+    def __init__(self, config: Config):
+        print("LppCollector.__init__() called")
+
+    def pytest_runtest_logreport(self, report: TestReport):
+        if report.when == "teardown":
+            print(
+                "LppCollector.pytest_runtest_logreport() called",
+                report.nodeid,
+                report.outcome,
+            )
+
+    def pytest_collection_modifyitems(self, items):
+        print("LppCollector.pytest_collection_modifyitems() called", items)
+
+
+def pytest_configure(config: Config):  # pragma: no cover
+    config.pluginmanager.register(LppCollector(config))
