4d89b67b1b96bc132906f43254b96d08a3f175bf
f0reachARR <f0reach@f0reach.me>
2024-07-21 14:22:07 +0900

    Add consent form

--- scripts/lpp_collector/lpp_collector/consent.py
+++ scripts/lpp_collector/lpp_collector/consent.py
@@ -1,6 +1,61 @@
 # Consent state management
 
 import os
-from .config import LPP_CONFIG_DIR, LPP_DATA_DIR
+from .config import LPP_DATA_DIR, LPP_CONSENT_TEXT
+from json import load
+import typing
+from whiptail import Whiptail
 
-LPP_CONSENT_FILE = os.path.join(LPP_CONFIG_DIR, "consent.json")
+LPP_CONSENT_FILE = os.path.join(LPP_DATA_DIR, "consent.json")
+
+
+class ConsentData(typing.TypedDict):
+    personal_id: str
+    device_id: str
+
+
+class LppExperimentConsent:
+    def __init__(self):
+        self._consent: typing.Optional[ConsentData] = None
+        self._load_consent()
+
+    def _load_consent(self):
+        try:
+            if os.path.exists(LPP_CONSENT_FILE):
+                with open(LPP_CONSENT_FILE, "r") as f:
+                    self._consent = load(f)
+        except Exception as e:
+            print(f"Failed to load experiment consent (unexpected error): {e}")
+
+    def get_consent(self):
+        return self._consent
+
+    def set_consent(self, consent: ConsentData):
+        self._consent = consent
+        try:
+            with open(LPP_CONSENT_FILE, "w") as f:
+                f.write(consent)
+        except Exception as e:
+            print(f"Failed to save experiment consent (unexpected error): {e}")
+
+    def delete_consent(self):
+        self._consent = None
+        os.remove(LPP_CONSENT_FILE)
+
+
+def main():
+    # Ask for consent using whiptail
+    whiptail = Whiptail(title="Consent Form for experiment")
+    consent = whiptail.run(
+        "yesno",
+        LPP_CONSENT_TEXT,
+        extra_args=[
+            "--scrolltext",
+            "--yes-button",
+            "同意する",
+            "--no-button",
+            "同意しない",
+        ],
+    )
+
+    print(f"Consent: {consent}")
