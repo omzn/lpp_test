b752861bf069f75f9029ea1e524bfdc023e40fc3
f0reachARR <f0reach@f0reach.me>
2024-09-30 17:50:59 +0900

    Fix python3.6 types

--- lpp_collector/sel_client/types.py
+++ lpp_collector/sel_client/types.py
@@ -1,6 +1,12 @@
 """ Contains some shared types for properties """
+
 from http import HTTPStatus
-from typing import Any, BinaryIO, Generic, MutableMapping, Optional, Tuple, TypeVar, Literal
+from typing import Any, BinaryIO, Generic, MutableMapping, Optional, Tuple, TypeVar
+
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal
 
 from attrs import define
 
@@ -17,14 +23,14 @@ FileJsonType = Tuple[Optional[str], BinaryIO, Optional[str]]
 
 @define
 class File:
-    """ Contains information for file uploads """
+    """Contains information for file uploads"""
 
     payload: BinaryIO
     file_name: Optional[str] = None
     mime_type: Optional[str] = None
 
     def to_tuple(self) -> FileJsonType:
-        """ Return a tuple representation that httpx will accept for multipart/form-data """
+        """Return a tuple representation that httpx will accept for multipart/form-data"""
         return self.file_name, self.payload, self.mime_type
 
 
@@ -33,7 +39,7 @@ T = TypeVar("T")
 
 @define
 class Response(Generic[T]):
-    """ A response from an endpoint """
+    """A response from an endpoint"""
 
     status_code: HTTPStatus
     content: bytes
